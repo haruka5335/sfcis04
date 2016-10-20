6b7f313642c344b131d84ec2a1dbe37702fe63ce
haruka5335 <Haruka5335@hayakawaharuka-no-MacBook-Air.local>
2016-10-20 10:53:15 +0900

    情報基礎　第4回　練習問題2,3
    練習問題2と3をやった！
    自作関数の定義の仕方、関数の中で関数を使うことをやった！

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+
+練習問題2
+
+われ
+function div(ここに引数を入れる){
+let a , b;　ここは返り値の変数を定義？ (let result)
+result = a / b; 割ろうね
+return result;　出そうね
+}
+
+先生
+function div(warareru, waru){
+let result;
+result = warareru / waru;
+return result;
+}
+
+
+練習問題3
+
+われ
+function toMile(warareru){ Kmをマイルに直す⇨kmってした方が楽だよね
+let result;
+waru = 1.6　ココいらない、なぜなら
+result = div　ここでdiv(km, 1.6)と定義するため
+return result}
+
+先生
+function toMile(km){
+    let result;
+    result = div(km, 1.6);
+    return result;
+}
